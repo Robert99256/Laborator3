06d60db82100dc73937e3ac06c61722acba553ae
Robert99256 <robertgabriel.popoviciu@ulbsibiu.ro>
2025-05-26 20:05:18 +0300

    commit comentarii

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,40 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.HashSet;
 
-public class Main{
+import java.util.ArrayList; // Pentru lista dinamica (List)
+import java.util.List;      // Interfața List
+import java.util.Random;    // Pentru generarea numerelor aleatorii
+import java.util.HashSet;   // Pentru setul de tip HashSet (fără duplicate)
+
+
+public class Main {
     public static void main(String[] args) {
-        List<Integer> numere=new ArrayList<>();
-        Random random=new Random();
-        for(int i=0;i<15;i++){
-            numere.add(random.nextInt(100));
+
+        // Creează o listă de numere întregi (goală inițial)
+        List<Integer> numere = new ArrayList<>();
+
+        // Creează un obiect Random pentru generare de numere aleatoare
+        Random random = new Random();
+
+        // Generează 15 numere întregi aleatoare între 0 și 99 și le adaugă în listă
+        for (int i = 0; i < 15; i++) {
+            numere.add(random.nextInt(100)); // random.nextInt(100) generează un număr între 0 și 99
         }
-        for(int num:numere){
-            System.out.println(num);
+
+        // Parcurge lista de numere și le afișează în consolă
+        for (int num : numere) {
+            System.out.println(num); // Afișează fiecare număr generat
         }
-        HashSet<String> set=new HashSet<>();
+
+        // Creează un set HashSet pentru a stoca valori de tip String (nu permite duplicate)
+        HashSet<String> set = new HashSet<>();
+
+        // Adaugă două elemente în set
         set.add("TEST1");
         set.add("TEST2");
-        for(String element:set){
-            System.out.println(element);
+
+
+        // Parcurge și afișează toate elementele din set
+        for (String element : set) {
+            System.out.println(element); // Ordinea poate fi diferită de cea de inserare
         }
     }
 }
